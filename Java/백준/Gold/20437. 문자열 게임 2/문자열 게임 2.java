102d961e5303be6050a647315576c534a1b7844a
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-05-19 16:20:53 +0900

    [Gold V] Title: 문자열 게임 2, Time: 312 ms, Memory: 33172 KB -BaekjoonHub

--- Java/백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ Java/백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,42 +1,45 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void solution(String W, int K) {
-        int size = W.length();
-        int[] alphabetCnt = new int[26];
-
-        for (char c : W.toCharArray()) alphabetCnt[c - 'a']++;
-
         int min = Integer.MAX_VALUE;
         int max = -1;
-        for (int i = 0; i < size; i++) {
-            char c = W.charAt(i);
-            if (alphabetCnt[c - 'a'] < K) continue;
 
-            int cnt = 1;
-            for (int j = i + 1; j < size; j++) {
-                if (c == W.charAt(j)) cnt++;
-                if (cnt == K) {
-                    min = Math.min(min, j - i + 1);
-                    max = Math.max(max, j - i + 1);
-                    break;
-                }
+        // 각 알파벳마다 등장 위치 저장
+        List<Integer>[] positions = new ArrayList[26];
+        for (int i = 0; i < 26; i++) positions[i] = new ArrayList<>();
+
+        for (int i = 0; i < W.length(); i++) {
+            char c = W.charAt(i);
+            positions[c - 'a'].add(i);
+        }
+
+        // 각 알파벳마다 슬라이딩 윈도우 적용
+        for (int i = 0; i < 26; i++) {
+            List<Integer> list = positions[i];
+            if (list.size() < K) continue;
+
+            for (int j = 0; j <= list.size() - K; j++) {
+                int len = list.get(j + K - 1) - list.get(j) + 1;
+                min = Math.min(min, len);
+                max = Math.max(max, len);
             }
         }
 
         System.out.println(max == -1 ? -1 : min + " " + max);
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
+        while (T-- > 0) {
             String W = br.readLine();
             int K = Integer.parseInt(br.readLine());
 
-            if(K == 1) {
+            if (K == 1) {
                 System.out.println("1 1");
                 continue;
             }
