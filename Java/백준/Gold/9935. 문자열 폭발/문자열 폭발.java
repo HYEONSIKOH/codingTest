c3c231d6a2e754148e0760ee24e4e488ee04259d
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-06-06 08:44:03 +0900

    [Gold IV] Title: 문자열 폭발, Time: 436 ms, Memory: 35752 KB -BaekjoonHub

---
+++ Java/백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    private static void solutuion(String str, String explosion) {
+        char[] cs = str.toCharArray();
+        char[] es = explosion.toCharArray();
+
+        Stack<Character> s = new Stack<>();
+
+        for (char c : cs) {
+            s.push(c);
+            int eLen = explosion.length();
+
+            boolean boom = false;
+            if (s.size() >= eLen) {
+                for (int i = 0; i < eLen; i++) {
+                    if (s.get(s.size() - 1 - i).equals(es[eLen - 1 - i]))
+                        boom = true;
+                    else {
+                        boom = false;
+                        break;
+                    }
+                }
+            }
+
+            if (boom) {
+                for (int i = 0; i < eLen; i++) {
+                    s.pop();
+                }
+            }
+        }
+
+        /**
+         * String에 문자를 추가하게 되면, 매번 새로운 String 객체가 생성 -> MemoryOverFlow
+         * StringBuilder를 사용 -> StringBuilder는 내부적으로 char[]를 사용하여 메모리 효율적
+         */
+        if (s.isEmpty()) {
+            System.out.print("FRULA");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (char c : s) sb.append(c);
+            System.out.print(sb);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String str = br.readLine();
+        String explosion = br.readLine();
+
+        solutuion(str, explosion);
+    }
+}
