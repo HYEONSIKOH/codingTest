b92e117cbdaec3dfd951993e86f980a7efbb03ab
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-06-16 14:26:33 +0900

    [Gold III] Title: 파티, Time: 172 ms, Memory: 17984 KB -BaekjoonHub

--- Java/백준/Gold/1238. 파티/파티.java
+++ Java/백준/Gold/1238. 파티/파티.java
@@ -5,6 +5,12 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
 
+    private static List<Node>[] village;
+    private static List<Node>[] reversedVillage;
+
+    private static int[] reverseDist;
+    private static int[] dist;
+
     private static class Node implements Comparable<Node>{
         int cur;
         int dist;
@@ -20,8 +26,8 @@ public class Main {
         }
     }
 
-    private static int dijkstra (int N, int s, int x, List<Node>[] village) {
-        int[] dist = new int[N];
+    private static void dijkstra (int N, int s) {
+        dist = new int[N];
         Arrays.fill(dist, Integer.MAX_VALUE);
         PriorityQueue<Node> pq = new PriorityQueue<>();
 
@@ -40,15 +46,38 @@ public class Main {
                 }
             }
         }
-
-        return dist[x];
     }
 
-    private static void solution(int N, int X, List<Node>[] village) {
+    private static void reverseDijkstra (int N, int s) {
+        reverseDist = new int[N];
+        Arrays.fill(reverseDist, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        pq.add(new Node(s, 0));
+        reverseDist[s] = 0;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+
+            for (Node nextNode : reversedVillage[node.cur]) {
+                int nextCur = nextNode.cur;
+                int nextDist = node.dist + nextNode.dist;
+
+                if (nextDist < reverseDist[nextCur]) {
+                    reverseDist[nextCur] = nextDist;
+                    pq.add(new Node(nextCur, nextDist));
+                }
+            }
+        }
+    }
+
+    private static void solution(int N, int X) {
         int ans = -1;
+        reverseDijkstra(N, X);
+        dijkstra(N, X);
+
         for (int i = 0; i < N; i++) {
             if (i != X)
-                ans = Math.max(ans, dijkstra(N, i, X, village) + dijkstra(N, X, i, village));
+                ans = Math.max(ans, dist[i] + reverseDist[i]);
         }
 
         System.out.println(ans);
@@ -60,9 +89,12 @@ public class Main {
         int M = Integer.parseInt(st.nextToken()); // M개의 도로 개수
         int X = Integer.parseInt(st.nextToken()); // 파티를 할 마을번호?
 
-        List<Node>[] village = new ArrayList[N];
-        for (int i = 0; i < N; i++)
+        village = new ArrayList[N];
+        reversedVillage = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
             village[i] = new ArrayList<>();
+            reversedVillage[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -72,8 +104,9 @@ public class Main {
             int t = Integer.parseInt(st.nextToken());
 
             village[s].add(new Node(e, t));
+            reversedVillage[e].add(new Node(s, t));
         }
 
-        solution(N, X - 1, village);
+        solution(N, X - 1);
     }
 }
