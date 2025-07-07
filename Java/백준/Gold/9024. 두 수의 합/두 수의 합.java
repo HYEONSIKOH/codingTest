797b6e0ff6ae8ae3cc6e4938d3ff580f83679b1a
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-07-07 14:45:17 +0900

    [Gold V] Title: 두 수의 합, Time: 1864 ms, Memory: 230168 KB -BaekjoonHub

---
+++ Java/백준/Gold/9024. 두 수의 합/두 수의 합.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int solution(int n, int k, int[] arr) {
+        Arrays.sort(arr);
+
+        int L = 0, R = n - 1;
+        int ans = 1, min = Integer.MAX_VALUE;
+        while (L < R) {
+            int sum = arr[L] + arr[R];
+            int absKMin = Math.abs(sum - k);
+
+            if (absKMin < min) {
+                min = absKMin;
+                ans = 1;
+            } else if (absKMin == min) ans++;
+
+            if (sum > k) R--;
+            else if (sum < k) L++;
+            else {
+                R--;
+                L++;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+        while(cnt-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            int[] arr = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++)
+                arr[i] = Integer.parseInt(st.nextToken());
+
+            bw.write(solution(n, k, arr) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
