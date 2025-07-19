38d75e2e029d1d7ffadc3886eebcdabcff21e56f
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-07-19 15:53:25 +0900

    [Gold IV] Title: 최단경로, Time: 724 ms, Memory: 120824 KB -BaekjoonHub

--- Java/백준/Gold/1753. 최단경로/최단경로.java
+++ Java/백준/Gold/1753. 최단경로/최단경로.java
@@ -3,37 +3,27 @@ import java.io.*;
 
 public class Main {
 
-    private static class Edge {
-        int to;
-        int cost;
-
-        Edge(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
-        }
-    }
-
-    private static int[] solution(int V, int E, int K, List<Edge>[] arr) {
+    private static int[] solution(int V, int E, int K, List<int[]>[] arr) {
         int[] dist = new int[V + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
 
         dist[K] = 0;
-        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
 
-        pq.add(new Edge(K, 0));
+        pq.add(new int[]{K, 0});
         while (!pq.isEmpty()) {
-            Edge cur = pq.poll();
+            int[] cur = pq.poll();
 
             // 이미 처리된 노드라면 continue
-            if (cur.cost > dist[cur.to]) continue;
+            if (cur[1] > dist[cur[0]]) continue;
 
-            for (int i = 0; i < arr[cur.to].size(); i++) {
-                Edge next = arr[cur.to].get(i);
-                int nextCost = cur.cost + next.cost;
+            for (int i = 0; i < arr[cur[0]].size(); i++) {
+                int[] next = arr[cur[0]].get(i);
+                int nextCost = cur[1] + next[1];
 
-                if (nextCost < dist[next.to]) {
-                    dist[next.to] = nextCost;
-                    pq.add(new Edge(next.to, nextCost));
+                if (nextCost < dist[next[0]]) {
+                    dist[next[0]] = nextCost;
+                    pq.add(new int[]{next[0], nextCost});
                 }
             }
         }
@@ -52,14 +42,14 @@ public class Main {
 
         int K = Integer.parseInt(br.readLine()); // 시작 정점
 
-        List<Edge>[] arr = new ArrayList[V + 1];
+        List<int[]>[] arr = new ArrayList[V + 1];
         for (int i = 0; i < V + 1; i++)
             arr[i] = new ArrayList<>();
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             arr[Integer.parseInt(st.nextToken())]
-                    .add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+                    .add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
 
         int[] res = solution(V, E, K, arr);
