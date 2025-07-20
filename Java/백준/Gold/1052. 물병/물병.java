f7cdd59c6caa3261139cdb664ae51e1c685287e2
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-07-20 18:31:14 +0900

    [Gold V] Title: 물병, Time: 84 ms, Memory: 11616 KB -BaekjoonHub

---
+++ Java/백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int solution(int N, int K) {
+        int ans = 0;
+        while (Integer.bitCount(N) > K) {
+            N++;
+            ans++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(solution(N, K)));
+
+        // System.gc();
+        bw.flush();
+        bw.close();
+    }
+}
