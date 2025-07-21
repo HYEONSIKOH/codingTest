806235d0d84c6117486b1b6fc3b88949a7ef71ad
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-07-21 17:53:12 +0900

    [Gold V] Title: 링크와 스타트, Time: 1000 ms, Memory: 15772 KB -BaekjoonHub

--- Java/백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ Java/백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -3,40 +3,34 @@ import java.io.*;
 
 public class Main {
 
-    private static int getSum(List<Integer> list, int[][] arr) {
-        int result = 0 ;
+    private static int[][] arr;
+    private static boolean[] visited;
+    private static int answer = Integer.MAX_VALUE;
 
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                result += arr[list.get(i)][list.get(j)];
-                result += arr[list.get(j)][list.get(i)];
-            }
-        }
-
-        return result;
+    private static void solution(int N) {
+        visited = new boolean[N];
+        dfs(0, 0, N);
     }
 
-    private static int solution(int N, int[][] arr) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i < Math.pow(2, N); i++) {
-            char[] bin = Integer.toBinaryString(i).toCharArray();
-
-            List<Integer> start = new ArrayList<>(); // 0은 스타트
-            List<Integer> link = new ArrayList<>();  // 1은 링크
-            for (int j = 0; j < N; j++) {
-                if (j + 1 > bin.length){
-                    start.add(j);
-                    continue;
+    private static void dfs(int idx, int count, int N) {
+        if (count >= 1 && count < N) {
+            int start = 0, link = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (visited[i] && visited[j]) start += arr[i][j];
+                    else if (!visited[i] && !visited[j]) link += arr[i][j];
                 }
-
-                if (bin[j] == '1') link.add(j);
-                else start.add(j);
             }
-
-            min = Math.min(min, Math.abs(getSum(start, arr) - getSum(link, arr)));
+            answer = Math.min(answer, Math.abs(start - link));
         }
 
-        return min;
+        for (int i = idx; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i + 1, count + 1, N);  // idx는 i+1로, count는 +1
+                visited[i] = false;
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -45,14 +39,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][N];
+        arr = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        bw.write(String.valueOf(solution(N, arr)));
+        solution(N);
+        bw.write(String.valueOf(answer));
 
         // System.gc();
         bw.flush();
