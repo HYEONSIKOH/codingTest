bd5e7dfcff0fdc851c06c4e361cec6d0bbd19798
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-07-25 08:47:59 +0900

    [Gold IV] Title: N-Queen, Time: 2756 ms, Memory: 12200 KB -BaekjoonHub

--- Java/백준/Gold/9663. N－Queen/N－Queen.java
+++ Java/백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,37 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static boolean[][] visited;
+    private static boolean[] column;
+    private static boolean[] positive;
+    private static boolean[] negative;
     private static int ans = 0;
 
-    private static void solution(int row, int N) {
-        if (row == N) {
+    private static void solution(int depth, int N) {
+        if (depth == N) {
             ans++;
             return;
         }
 
-        for (int col = 0; col < N; col++) {
-            if (isValid(row, col, N)) {
-                visited[row][col] = true;
-                solution(row + 1, N);
-                visited[row][col] = false;
+        for (int i = 0; i < N; i++) {
+            if (!column[i] && !positive[depth + i] && !negative[depth - i + N]) {
+                column[i] = true;
+                positive[depth + i] = true;
+                negative[depth - i + N] = true;
+                solution(depth + 1, N);
+                column[i] = false;
+                positive[depth + i] = false;
+                negative[depth - i + N] = false;
             }
         }
-    }
-
-    private static boolean isValid(int row, int col, int N) {
-        // 위쪽 대각선 왼쪽
-        for (int i = 1; i <= row; i++)
-            if (col - i >= 0 && visited[row - i][col - i]) return false;
-
-        // 위쪽 대각선 오른쪽
-        for (int i = 1; i <= row; i++)
-            if (col + i < N && visited[row - i][col + i]) return false;
-
-        // 위쪽 직선만 확인
-        for (int i = 0; i < row; i++) if (visited[i][col]) return false;
-
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,7 +31,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        visited = new boolean[N][N];
+        column = new boolean[N];
+        positive = new boolean[2 * N];
+        negative = new boolean[2 * N];
 
         solution(0, N);
         bw.write(String.valueOf(ans));
