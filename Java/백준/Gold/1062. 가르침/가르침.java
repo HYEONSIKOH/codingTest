07a82c42199f3348f8d8a05c67b9147a45208cff
오현식 <100890581+HYEONSIKOH@users.noreply.github.com>
2025-07-27 17:29:14 +0900

    [Gold IV] Title: 가르침, Time: 168 ms, Memory: 12040 KB -BaekjoonHub

--- Java/백준/Gold/1062. 가르침/가르침.java
+++ Java/백준/Gold/1062. 가르침/가르침.java
@@ -4,60 +4,38 @@ import java.io.*;
 import static java.lang.Integer.parseInt;
 
 public class Main {
-    private static boolean[] visited = new boolean[26];
-    private static int bitmask = 0;
     private static int ans = -1;
 
     private static int solution(int N, int K, String[] words){
         if (K < 5) return 0;
 
-        int[] newWards = new int[N];
+        int[] masks = new int[N];
         for (int i = 0; i < N; i++) {
-            String word = words[i];
-            int bitmask = 0;
-
-            for (char c : word.toCharArray()) {
-                bitmask |= (1 << (c - 'a'));
-            }
-
-            newWards[i] = bitmask;
+            for (char c : words[i].toCharArray())
+                masks[i] |= 1 << (c - 'a');
         }
 
-        visited['a' - 'a'] = true;
-        visited['n' - 'a'] = true;
-        visited['t' - 'a'] = true;
-        visited['i' - 'a'] = true;
-        visited['c' - 'a'] = true;
+        int base = 0;
+        for (char c : "antic".toCharArray())
+            base |= 1 << (c - 'a');
 
-        for (int i = 0; i < 26; i++) {
-            if (visited[i]) bitmask |= (1 << i);
-        }
-
-        backtrack(-1, 0, K - 5, newWards);
+        backtrack(0, 0, K - 5, base, masks);
 
         return ans;
     }
 
-    private static void backtrack(int idx, int cnt, int K, int[] words) {
+    private static void backtrack(int idx, int cnt, int K, int bitmask, int[] words) {
         if (cnt > K) return;
 
-        bitmask = 0;
-        for (int i = 0; i < 26; i++) {
-            if (visited[i]) bitmask |= (1 << i);
-        }
+        ans = Math.max(ans, cntValid(bitmask, words));
 
-        ans = Math.max(ans, isValid(words));
-
-        for (int i = idx + 1; i < 26; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                backtrack(i, cnt + 1, K, words);
-                visited[i] = false;
-            }
+        for (int i = idx; i < 26; i++) {
+            if ((bitmask & (1 << i)) == 0)
+                backtrack(i + 1, cnt + 1, K, bitmask | (1 << i), words);
         }
     }
 
-    private static int isValid(int[] words) {
+    private static int cntValid(int bitmask, int[] words) {
         int cnt = 0;
 
         for (int word : words) {
