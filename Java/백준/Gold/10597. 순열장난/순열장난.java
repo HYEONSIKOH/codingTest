2b4bec06cd3e2172f4635743bc9f530c2c1d097c
오현식 <ks06891@naver.com>
2025-08-07 16:02:54 +0900

    [Gold V] Title: 순열장난, Time: 144 ms, Memory: 14596 KB -BaekjoonHub

--- Java/백준/Gold/10597. 순열장난/순열장난.java
+++ Java/백준/Gold/10597. 순열장난/순열장난.java
@@ -1,17 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-import static java.lang.Integer.*;
-
 public class Main {
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static StringBuilder sb;
     private static int size;
     private static int range;
 
     private static char[] arr;
     private static boolean[] visited;
+    private static int[] ans;
 
     private static void solution(){
         size = arr.length;
@@ -19,36 +17,39 @@ public class Main {
         // 가장 큰 수 구하기
         if (size > 10) range = (size - 10) / 2 + 10;
         else range = size;
-
-        sb = new StringBuilder();
+        
         visited = new boolean[range + 1];
+        ans = new int[range];
         for (int i = 1; i <= range; i++) {
             if (i < 10) {
                 if (arr[0] - '0' == i) {
-                    sb.append(i).append(" ");
+                    ans[0] = i;
                     visited[i] = true;
-                    backtrack(0);
+                    backtrack(0, 0);
                     visited[i] = false;
-                    sb.delete(sb.length() - 2, sb.length());
                 }
             }
 
             if (i >= 10){
                 int num = (arr[0] - '0') * 10 + (arr[1] - '0');
                 if (num == i) {
-                    sb.append(num).append(" ");
+                    ans[0] = i;
                     visited[i] = true;
-                    backtrack(1);
+                    backtrack(1, 0);
                     visited[i] = false;
-                    sb.delete(sb.length() - 3, sb.length());
                 }
             }
 
         }
     }
 
-    private static void backtrack(int idx){
+    private static void backtrack(int idx, int num){
         if (idx == size - 1) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i : ans)
+                sb.append(i).append(" ");
+
             System.out.println(sb.toString());
             System.exit(0);
         }
@@ -57,22 +58,19 @@ public class Main {
             if (visited[i]) continue;
             if (i < 10) {
                 if (arr[idx + 1] - '0' == i) {
-                    sb.append(i).append(" ");
+                    ans[num + 1] = i;
                     visited[i] = true;
-                    backtrack(idx + 1);
+                    backtrack(idx + 1, num + 1);
                     visited[i] = false;
-                    sb.delete(sb.length() - 2, sb.length());
                 }
             }
 
             if (idx + 2 < size && i >= 10) {
-                int num = (arr[idx + 1] - '0') * 10 + (arr[idx + 2] - '0');
-                if (num == i) {
-                    sb.append(num).append(" ");
+                if ((arr[idx + 1] - '0') * 10 + (arr[idx + 2] - '0') == i) {
+                    ans[num + 1] = i;
                     visited[i] = true;
-                    backtrack(idx + 2);
+                    backtrack(idx + 2, num + 1);
                     visited[i] = false;
-                    sb.delete(sb.length() - 3, sb.length());
                 }
             }
         }
