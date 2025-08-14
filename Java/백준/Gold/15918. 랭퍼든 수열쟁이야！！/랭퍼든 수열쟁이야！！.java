c35e0bba51c0d8de5e5765764fc678f9a6f1894e
오현식 <ks06891@naver.com>
2025-08-15 08:38:03 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 240 ms, Memory: 12116 KB -BaekjoonHub

--- Java/백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
+++ Java/백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
@@ -22,26 +22,18 @@ public class Main {
         arr = new int[(N * 2) + 1];
         Arrays.fill(arr, -1);
 
-        for (int i = 1; i <= N; i++) {
-            if (2 + i <= 2 * N) {
-                arr[1] = i;
-                arr[2 + i] = i;
-                backtrack(1, X, Y, (1 << i));
-                arr[1] = -1;
-                arr[2 + i] = -1;
-            }
-        }
+        arr[X] = Y - X - 1;
+        arr[Y] = Y - X - 1;
 
+        int visited = (1 << (Y - X - 1));
+
+        backtrack(1, visited);
         return ans;
     }
 
-    private static void backtrack(int idx, int X, int Y, int visited) {
+    private static void backtrack(int idx, int visited) {
         while(idx <= 2 * N && arr[idx] != -1)
             idx++;
-
-        // X번째와 Y번째가 같은 수가 아닌 경우
-        if (arr[X] != -1 && arr[Y] != -1 && arr[X] != arr[Y])
-            return;
 
         if (idx >= 2 * N - 1 && visited == (1 << (N + 1)) - 2) {
             ans++;
@@ -54,7 +46,7 @@ public class Main {
                     && arr[i + idx + 1] == -1) {
                 arr[idx] = i;
                 arr[i + idx + 1] = i;
-                backtrack(idx + 1, X, Y, visited | (1 << i));
+                backtrack(idx + 1, visited | (1 << i));
                 arr[idx] = -1;
                 arr[i + idx + 1] = -1;
             }
