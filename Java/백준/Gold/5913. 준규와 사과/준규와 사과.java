3888309faf9db5d5120c8afb2ad8255e1e18a8c0
오현식 <ks06891@naver.com>
2025-08-25 15:19:52 +0900

    [Gold IV] Title: 준규와 사과, Time: 76 ms, Memory: 11572 KB -BaekjoonHub

--- Java/백준/Gold/5913. 준규와 사과/준규와 사과.java
+++ Java/백준/Gold/5913. 준규와 사과/준규와 사과.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static boolean[][] visited = new boolean[5][5];
-
     private static final int[] dx = {1, 0, -1, 0};
     private static final int[] dy = {0, 1, 0, -1};
 
@@ -15,32 +13,27 @@ public class Main {
         // StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(br.readLine());
+        int visited = 1;
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            visited[x-1][y-1] = true;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            visited |= (1 << (x * maxSize + y));
         }
 
-        visited[0][0] = true;
-
-        backtrack(0, 0);
+        backtrack(0, 0, visited);
         System.out.print(ans);
     }
 
     private static boolean isValid(int x, int y) {
-        return 0 <= x && x < maxSize && 0 <= y && y < maxSize && !visited[x][y];
+        return 0 <= x && x < maxSize && 0 <= y && y < maxSize;
     }
 
-    private static void backtrack(int x, int y) {
+    private static void backtrack(int x, int y, int visited) {
         if (x == 4 && y == 4) {
-            for (boolean[] row : visited) {
-                for (boolean cell : row) {
-                    if (!cell) return;
-                }
-            }
-            ans++;
+            if (visited == (1 << 25) - 1)
+                ans++;
             return;
         }
 
@@ -48,11 +41,9 @@ public class Main {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (isValid(nx, ny)) {
-                visited[nx][ny] = true;
-                backtrack(nx, ny);
-                visited[nx][ny] = false;
-            }
+            int index = nx * maxSize + ny;
+            if (isValid(nx, ny) && (visited & (1 << index)) == 0)
+                backtrack(nx, ny, visited | (1 << index));
         }
     }
 }
