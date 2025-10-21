415bccdedced3eb66bf8be80faf9d19b81b67dbd
오현식 <hyeonsik0407@gmail.com>
2025-10-21 22:12:30 +0900

    [Silver III] Title: 프린터 큐, Time: 96 ms, Memory: 12848 KB -BaekjoonHub

---
+++ Java/백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	private static int T, N, M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		T = Integer.parseInt(br.readLine());
+ 
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			LinkedList<int[]> q = new LinkedList<>();
+ 			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++)
+				q.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
+ 
+			int count = 0;
+			while (!q.isEmpty()) {
+				int[] front = q.poll();
+				boolean isMax = true;
+				 
+				for (int i = 0; i < q.size(); i++) {
+					if (front[1] < q.get(i)[1]) {
+						
+						q.offer(front);
+						for (int j = 0; j < i; j++)
+							q.offer(q.poll());
+                        
+						isMax = false;
+						break;
+					}
+				}
+				
+				if (isMax == false) continue;
+				
+				count++;
+				if (front[0] == M) break;
+			}
+ 
+			sb.append(count).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
