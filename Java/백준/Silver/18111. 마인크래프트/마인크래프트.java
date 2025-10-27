3df1ee72d62a00982225277df2d91b5bc8c9d459
오현식 <hyeonsik0407@gmail.com>
2025-10-27 15:21:11 +0900

    [Silver II] Title: 마인크래프트, Time: 744 ms, Memory: 34360 KB -BaekjoonHub

---
+++ Java/백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int[][] minecraft;
+
+    private static int N, M, B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        minecraft = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++)
+                minecraft[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] ans = solution();
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+
+    private static int[] solution() {
+        int[] ans = {Integer.MAX_VALUE, Integer.MIN_VALUE };
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                min = Math.min(min, minecraft[i][j]);
+                max = Math.max(max, minecraft[i][j]);
+            }
+        }
+
+        for (int idx = min; idx <= max; idx++) {
+            int time = 0, block = B;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (minecraft[i][j] > idx) {
+                        block += minecraft[i][j] - idx;
+                        time += 2 * (minecraft[i][j] - idx);
+                    }
+                }
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (minecraft[i][j] < idx) {
+                        block -= idx - minecraft[i][j];
+                        time += idx - minecraft[i][j];
+                    }
+                }
+                if (block < 0) break;
+            }
+
+            if (block >= 0) {
+                if (time < ans[0]) {
+                    ans[0] = time;
+                    ans[1] = idx;
+                } else if (time == ans[0] && idx > ans[1]) {
+                    ans[1] = idx;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
