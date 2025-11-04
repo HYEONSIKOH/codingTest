d6479fac94e6f0a80357235aaecc7363fd23c09c
오현식 <hyeonsik0407@gmail.com>
2025-11-04 21:48:34 +0900

    [Silver IV] Title: 스택, Time: 124 ms, Memory: 17228 KB -BaekjoonHub

---
+++ Java/백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static StringBuilder sb = new StringBuilder();
+    private static Deque<Integer> s = new ArrayDeque();
+    
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            
+            if (str.equals("push")) s.push(Integer.parseInt(st.nextToken()));
+
+            if (str.equals("pop")) {
+                if (s.isEmpty()) sb.append(-1 + "\n");
+                else sb.append(s.pop() + "\n");
+            }
+
+            if (str.equals("size")) sb.append(s.size() + "\n");
+
+            if (str.equals("empty")) {
+                if (s.isEmpty()) sb.append(1 + "\n");
+                else sb.append(0 + "\n");
+            }
+
+            if (str.equals("top")) {
+                if (s.isEmpty()) sb.append(-1 + "\n");
+                else sb.append(s.peek() + "\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
