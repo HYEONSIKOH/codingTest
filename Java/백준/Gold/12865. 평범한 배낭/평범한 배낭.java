151257425269379051e97c1a6030b043dae58d25
오현식 <hyeonsik0407@gmail.com>
2025-11-10 16:25:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51300 KB -BaekjoonHub

---
+++ Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	private static int[][] bags;
+	private static int N, K;
+
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	
+    	bags = new int[N][2];
+    	for (int i = 0; i < N; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		bags[i][0] = Integer.parseInt(st.nextToken());
+    		bags[i][1] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	System.out.print(solution());
+    }
+    
+    private static int solution() {
+    	int[][] dp = new int[N + 1][K + 1];
+    	
+    	for (int i = 1; i <= N; i++) {
+    		int WEIGHT = bags[i-1][0];
+    		int VALUE = bags[i-1][1];
+    		
+    		for (int j = 1; j <= K; j++) {
+    			if (WEIGHT <= j) dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - WEIGHT] + VALUE);
+    			else dp[i][j] = dp[i - 1][j];
+    		}
+    	}
+    	
+    	return dp[N][K];
+    }
+}
