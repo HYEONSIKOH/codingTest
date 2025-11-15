f9e36c844563fb6ae24f1bbed4bdcbef3367bfa7
오현식 <hyeonsik0407@gmail.com>
2025-11-15 18:37:26 +0900

    [Silver III] Title: 숫자 야구, Time: 100 ms, Memory: 19628 KB -BaekjoonHub

--- Java/백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ Java/백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -53,10 +53,11 @@ public class Main {
     }
     
     private static boolean isDueple(int num) {
-    	char[] s1 = Integer.toString(num).toCharArray();
+    	int[] s1 = decom(num);
     	
     	for (int i = 0; i < 3; i++) {
-    		if (s1[i] == '0') return false;
+    		if (s1[i] == 0) return false;
+    		
     		for (int j = i + 1; j < 3; j++)
     			if (s1[i] == s1[j])
     				return false;
@@ -68,8 +69,8 @@ public class Main {
     private static int[] playball(int n1, int n2) {
     	int s = 0, b = 0;
     	
-    	char[] s1 = Integer.toString(n1).toCharArray();
-    	char[] s2 = Integer.toString(n2).toCharArray();
+    	int[] s1 = decom(n1);
+    	int[] s2 = decom(n2);
     	
     	for (int i = 0; i < 3; i++) {
     		if (s1[i] == s2[i]) s++;
@@ -82,5 +83,19 @@ public class Main {
     	}
     	
     	return new int[]{s, b};
+    }
+    
+    private static int[] decom(int num) {
+    	int[] ans = new int[3];
+    	
+    	ans[2] = num % 10;
+    	num /= 10;
+    	
+    	ans[1] = num % 10;
+    	num /= 10;
+    	
+    	ans[0] = num;
+    	
+    	return ans;
     }
 }
