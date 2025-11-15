4bf03ed39fbb3ec298ac209f0165f6a738545c42
오현식 <hyeonsik0407@gmail.com>
2025-11-15 18:31:18 +0900

    [Silver III] Title: 숫자 야구, Time: 136 ms, Memory: 28836 KB -BaekjoonHub

---
+++ Java/백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	private static int[][] arr;
+	private static int N;
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	N = Integer.parseInt(br.readLine());
+    	arr = new int[N][];
+    	for (int i = 0; i < N; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		
+    		int number = Integer.parseInt(st.nextToken());
+    		int s = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		
+    		arr[i] = new int[]{number, s, b};
+    	}
+
+    	System.out.print(solution());
+    }
+    
+    private static int solution() {
+    	boolean[] visited = new boolean[1000];
+    	Arrays.fill(visited, true);
+    	
+    	for (int i = 102; i < 1000; i++)
+    		visited[i] = isDueple(i);
+    	
+    	for (int idx = 0; idx < N; idx++) {
+    		int n = arr[idx][0];
+    		int s = arr[idx][1];
+    		int b = arr[idx][2];
+    		
+    		for (int i = 102; i < 1000; i++) {
+    			int[] temp = playball(n, i);
+    			
+    			if (visited[i] && (temp[0] != s || temp[1] != b))
+    				visited[i] = false;
+    		}
+    	}
+    	
+    	int cnt = 0;
+    	for (int i = 102; i < 1000; i++) {
+    		if (visited[i]) cnt++;
+    	}
+    	
+    	return cnt;
+    }
+    
+    private static boolean isDueple(int num) {
+    	char[] s1 = Integer.toString(num).toCharArray();
+    	
+    	for (int i = 0; i < 3; i++) {
+    		if (s1[i] == '0') return false;
+    		for (int j = i + 1; j < 3; j++)
+    			if (s1[i] == s1[j])
+    				return false;
+    	}
+    	
+    	return true;
+    }
+    
+    private static int[] playball(int n1, int n2) {
+    	int s = 0, b = 0;
+    	
+    	char[] s1 = Integer.toString(n1).toCharArray();
+    	char[] s2 = Integer.toString(n2).toCharArray();
+    	
+    	for (int i = 0; i < 3; i++) {
+    		if (s1[i] == s2[i]) s++;
+    	}
+    	
+    	for (int i = 0; i < 3; i++) {
+    		for (int j = 0; j < 3; j++)
+    			if (i != j && s1[i] == s2[j])
+    				b++;
+    	}
+    	
+    	return new int[]{s, b};
+    }
+}
