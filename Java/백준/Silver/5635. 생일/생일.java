1156101bcfcf144ec61760421b166c4ee58a3419
오현식 <hyeonsik0407@gmail.com>
2025-11-16 16:23:08 +0900

    [Silver V] Title: 생일, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

--- Java/백준/Silver/5635. 생일/생일.java
+++ Java/백준/Silver/5635. 생일/생일.java
@@ -2,15 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	private static Object[][] arr;
+	private static Student[] arr;
 	private static int N;
+	
+	private static class Student implements Comparable<Student> {
+		String name;
+		int d, m, y;
+		
+		Student(String n, int d, int m, int y) {
+			this.name = n;
+			this.d = d;
+			this.m = m;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Student s) {
+			if (this.y != s.y) return this.y - s.y;
+            if (this.m != s.m) return this.m - s.m;
+            return this.d - s.d;
+		}
+	}
 	
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st;
     	
     	N = Integer.parseInt(br.readLine());
-    	arr = new Object[N][4];
+    	arr = new Student[N];
     	for (int i = 0; i < N; i++) {
     		st = new StringTokenizer(br.readLine());
     		
@@ -19,27 +38,15 @@ public class Main {
     		int m = Integer.parseInt(st.nextToken());
     		int y = Integer.parseInt(st.nextToken());
     		
-    		arr[i] = new Object[]{name, d, m, y};
+    		arr[i] = new Student(name, d, m, y);
     	}
 
     	System.out.print(solution());
     }
     
     private static String solution() {
-    	Arrays.sort(arr, (a, b) -> {
-    		int y1 = (Integer) a[3];
-            int y2 = (Integer) b[3];
-            if (y1 != y2) return Integer.compare(y1, y2);
-
-            int m1 = (Integer) a[2];
-            int m2 = (Integer) b[2];
-            if (m1 != m2) return Integer.compare(m1, m2);
-
-            int d1 = (Integer) a[1];
-            int d2 = (Integer) b[1];
-            return Integer.compare(d1, d2);
-    	});
+    	Arrays.sort(arr);
     	
-    	return arr[N-1][0] + "\n" + arr[0][0];
+    	return arr[N-1].name + "\n" + arr[0].name;
     }
 }
