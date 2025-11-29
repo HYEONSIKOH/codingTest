5f7a972fa19ec7bdd52adabf505609bb4e059569
오현식 <hyeonsik0407@gmail.com>
2025-11-29 17:32:51 +0900

    [Silver V] Title: 단어 나누기, Time: 72 ms, Memory: 12748 KB -BaekjoonHub

--- Java/백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ Java/백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -2,34 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    private static int N;
+    private static String str;
+    
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	//StringTokenizer st = new StringTokenizer(br.readLine());
     	
-    	String str = br.readLine();
+    	str = br.readLine();
     	
-    	System.out.print(solution(str));
+    	System.out.print(solution());
     }
     
-    private static String solution(String str) {
-    	int N = str.length();
+    private static String solution() {
+    	N = str.length();
     	String ans = null;
     	
     	for (int i = 1; i < N - 1; i++) {
     		for (int j = i + 1; j < N; j++) {
-    			String temp = function(i, j, str);
-    			if (ans == null || temp.compareTo(ans) < 0) {
+    			String temp = function(i, j);
+    			
+    			if (ans == null || temp.compareTo(ans) < 0)
     	            ans = temp;
-    	        }
     		}
     	}
     	
     	return ans;
     }
     
-    private static String function(int i, int j, String str) {
+    private static String function(int i, int j) {
     	StringBuilder sb = new StringBuilder();
-    	int N = str.length();
     	
     	sb.append(new StringBuilder(str.substring(0, i)).reverse());
     	sb.append(new StringBuilder(str.substring(i, j)).reverse());
